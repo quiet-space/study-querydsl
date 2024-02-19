1c236af2243c79d7fcfe084454f754e6998eddfa
Shin HyeJi <jssin97@gmail.com>
2024-02-19 18:48:04 +0900

    240219: Pageable

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -5,8 +5,13 @@ import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 
@@ -39,7 +44,7 @@ class MemberRepositoryTest {
 
 
     @Test
-    public void searchTest() {
+    void searchTest() {
         Member member = new Member("member1", 10);
 
         // extends(상속) method 를 호출
@@ -53,5 +58,57 @@ class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchPageSimple() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+
+            em.persist(teamA);
+            em.persist(teamB);
+
+            Member member1 = new Member("member1", 10, teamA);
+            Member member2 = new Member("member2", 20, teamA);
+
+            Member member3 = new Member("member3", 30, teamB);
+            Member member4 = new Member("member4", 40, teamB);
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+            em.persist(member4);
+
+            MemberSearchCondition condition = new MemberSearchCondition();
+            PageRequest pageRequest = PageRequest.of(0, 3);
+
+            Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
+            assertThat(result.getSize()).isEqualTo(3);
+    }
+
+    @Test
+    public void searchPageComplex() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageComplex(condition, pageRequest);
+        assertThat(result.getSize()).isEqualTo(3);
     }
 }
